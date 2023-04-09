2422c2d01c5ce2dd82060403a2e9deef1a66fe2f
Pratham8223 <prathameshbandal74@gmail.com>
2023-04-10 02:18:05 +0530

    Conditional Statements

---
+++ second.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int calculate = sc.nextInt();
+        // int b = sc.nextInt();
+
+        // if( a == b) {
+        //     System.out.println("both numbers are equal");
+        // }
+        // else if (a>b) {
+        //     System.out.println("a is greater");
+        // } 
+        // else {
+        //     System.out.println("a is smaller");
+        // }
+// switch block for greetings
+        // switch(a) {
+        //     case 1  : System.out.println("hello");
+        //     break;
+        //     case 2 : System.out.println("namaste");
+        //     break;
+        //     case 3 : System.out.println("bonjour");
+        //     break;
+        //     case 4 : System.out.println("hola");
+        //     break;
+        //     default : System.out.println("please enter a valid number between 1 and 4");
+        // }
+
+// switch block for calculation
+        // switch(calculate){
+        //     case 1 : System.out.println("The addition of the given 2 numbers is" +(a+b));
+        //     break;
+        //     case 2 : System.out.println("the substractio of the given two numbers is "+(a-b));
+        //     break;
+        //     case 3 : System.out.println("The multiplication of the given two numbers is " +(a*b));
+        //     break;
+        //     case 4 : System.out.println("the remainder of the given two numbers is" +(a%b));
+        //     break;
+        //     default : System.out.println("please enter valid symbol");
+        // }
+        switch(a) {
+            case 1 : System.out.println("Jan");
+            break;
+            case 2 : System.out.println("Feb");
+            break;
+            case 3 : System.out.println("Mar");
+            break;
+            case 4 : System.out.println("April");
+            break;
+            case 5 : System.out.println("May");
+            break;
+            case 6 : System.out.println("Jun");
+            break;
+            case 7 : System.out.println("July");
+            break;
+            case 8 : System.out.println("Aug");
+            break;
+            case 9 : System.out.println("Sept");
+            break;
+            case 10 : System.out.println("Oct");
+            break;
+            case 11 : System.out.println("Nov");
+            break;
+            case 12 : System.out.println("Dec");
+            break;
+            default : System.out.println("please enter month numbers from 1 to 12 only");
+        }
+
+        }
+        
+    }
